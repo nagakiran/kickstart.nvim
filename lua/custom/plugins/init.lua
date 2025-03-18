7dfec242ef4048486b59c136592901d8feafdcca
nagakiran <nagakiran@juniper.net>
2025-03-18 14:23:46 +0530

    feat: add CopilotChat.nvim plugin with mappings and keybindings
    - Added CopilotChat.nvim plugin to the configuration.
    - Configured custom mappings for completion.
    - Integrated file input using fzf-lua.
    - Added keybinding for CopilotChatToggle command.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -216,7 +216,31 @@ return {
     build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
+      mappings = {
+        complete = {
+          detail = 'Use @<Tab> or /<Tab> for options.',
+          insert = '<S-Tab>',
+        },
+      },
+      file = {
+        input = function(callback)
+          local fzf = require 'fzf-lua'
+          local fzf_path = require 'fzf-lua.path'
+          fzf.files {
+            complete = function(selected, opts)
+              local file = fzf_path.entry_to_file(selected[1], opts, opts._uri)
+              if file.path == 'none' then
+                return
+              end
+              vim.defer_fn(function()
+                callback(file.path)
+              end, 100)
+            end,
+          }
+        end,
+      },
     },
+    keys = { { '<leader>av', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChatToggle' } },
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
