feb25876cc02313e88c9c47b40d02d2c8cc7e69f
nagakiran <nagakiran@juniper.net>
2026-07-02 15:29:13 +0530

    feat(git): add git-conflict.nvim and fix fugitive dq keymap
    - Add akinsho/git-conflict.nvim for conflict marker navigation and resolution
    - Extend `dq` keymap in fugitive to close diff windows from any pane,
      fixing 3-way merge resolution where fugitive's native dq is unavailable
      on the working file side

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,5 +1,16 @@
 return {
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      -- Close all diff windows from any pane (fugitive's dq only exists on blob side panes,
+      -- not on the working file where you actually edit during 3-way merge resolution)
+      vim.keymap.set('n', 'dq', function()
+        if vim.wo.diff then
+          vim.fn['fugitive#DiffClose']()
+        end
+      end, { silent = true, desc = 'Close diff windows' })
+    end,
+  },
   {
     'shumphrey/fugitive-gitlab.vim', -- A vim extension to fugitive.vim for GitLab support
     config = function()
---
+++ lua/custom/plugins/git-conflict.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/git-conflict.nvim',
+  version = '*',
+  event = 'BufReadPost',
+  opts = {
+    default_mappings = true,
+    default_commands = true,
+    disable_diagnostics = false,
+  },
+}
